4a3e306070ac98edc0506cf48fe4c29570904c07
Lucas_Peter <notlucaspeterson@gmail.com>
2023-04-21 15:31:30 +0200

    Fix random

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def process_rules_command(message: Message):
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: Message):
     # Приветственное сообщение
-    await bot.send_message(message.chat.id, "Привет! Чтобы получить список ролей, напишите команду /role")
+    await bot.send_message(message.chat.id, "Привет! Чтобы получить список ролей, напишите команду /role, чтобы узнать колл-во ролей, можешь ввести команду /rules")
 
 
 @dp.message_handler()
@@ -38,10 +38,14 @@ async def process_message(message: Message):
         if num_players < 7:
             await bot.send_message(message.chat.id, "Слишком мало игроков.")
             return
-        elif num_players <= 8:
+        elif num_players == 7:
             roles = ["Мирный"] * 4 + ["Мафия", "Дон", "Шериф"]
-        elif num_players <= 10:
+        elif num_players == 8:
+            roles = ["Мирный"] * 5 + ["Мафия", "Дон", "Шериф"]
+        elif num_players == 9:
             roles = ["Мирный"] * 5 + ["Мафия"] * 2 + ["Дон", "Шериф"]
+        elif num_players == 10:
+            roles = ["Мирный"] * 6 + ["Мафия"] * 2 + ["Дон", "Шериф"]
         else:
             await bot.send_message(message.chat.id, "Слишком много игроков.")
             return
@@ -54,6 +58,7 @@ async def process_message(message: Message):
         for i, role in enumerate(roles):
             result_message += f"{i + 1}-{role}\n"
         await bot.send_message(message.chat.id, result_message)
+        #print(result_message)
     else:
         # Отправляем запрос на количество игроков
         await bot.send_message(message.chat.id, "Введите количество игроков:")
